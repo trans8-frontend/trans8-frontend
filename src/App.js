4d1ff7fcc1e05c5002d93c1792daaaefe8d30364
trans8-frontend <92789730+trans8-frontend@users.noreply.github.com>
2021-10-19 16:29:40 +0530

    layout

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        
+      </div>
+    </Router>
+  );
+}
+
+export default App;
